6f88aeac00962ff4135ed160dcc7344254a7daf9
xy1999729 <32480633+xy1999729@users.noreply.github.com>
2021-11-13 20:29:36 +0800

    Update main_orth34.py

--- imagenet/main_orth34.py
+++ imagenet/main_orth34.py
@@ -17,6 +17,7 @@ import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
+import torch.nn.functional as F
 import utils
 from pdb import set_trace as st
 from PIL import Image, ImageFile
