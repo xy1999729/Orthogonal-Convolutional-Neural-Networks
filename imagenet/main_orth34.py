01ad5870244348bcba2f468a77f99b2ea63d0f0c
xy1999729 <32480633+xy1999729@users.noreply.github.com>
2021-11-13 15:40:45 +0800

    Update main_orth34.py

--- imagenet/main_orth34.py
+++ imagenet/main_orth34.py
@@ -74,7 +74,7 @@ parser.add_argument('--seed', default=None, type=int,
                     help='seed for initializing training. ')
 parser.add_argument('--gpu', default=None, type=int,
                     help='GPU id to use.')
-parser.add_argument('--multiprocessing-distributed', action='store_true',
+parser.add_argument('--multiprocessing-distributed', action='store_true', default=true,
                     help='Use multi-processing distributed training to launch '
                          'N processes per node, which has N GPUs. This is the '
                          'fastest way to use PyTorch for either single node or '
